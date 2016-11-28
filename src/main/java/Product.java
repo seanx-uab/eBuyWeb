ae4f3d71f95c590178cd1c250afd023bd1f517aa
Alice <alice@gmail.com>
2016-11-28 22:59:11 +0100

    add isAvail instance variable for Project.java

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -2,15 +2,18 @@ public class Product {
 	
 	private String name;
 	private double price;
+	private boolean isAvail;
 	
 	public Product() {
 		this.name = "";
-		this.price = 0.0;	
+		this.price = 0.0;
+		this.isAvail = false;
 	}
 
-	public Product(name, price) {
+	public Product(String name, double price, boolean isAvail) {
 		this.name = name;
 		this.price = price;
+		this.isAvail = isAvail;
 	}
 
 }
